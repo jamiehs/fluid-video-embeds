9fb24e84cd4e1755b5ad52d9b6c5d8b63d87b7e4
Jamie Hamel-Smith <jamie3d@gmail.com>
2013-01-07 21:05:46 -0800

    Attempting to fix my erroneous merge while messing around

--- views/elements/_javascript_variables.php
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript">
-	var FluidVideoEmedsiFrameBeforeSrc = '<?php echo $this->iframe_before_src; ?>';
-	var FluidVideoEmedsiFrameAfterSrc = '<?php echo $this->iframe_after_src; ?>';
-</script>
